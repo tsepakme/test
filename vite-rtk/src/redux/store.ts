147318fb6aa87c9886183ea6a72aaf58cb767156
Aiusha <ayushamikhaylov@gmail.com>
2025-03-26 21:19:06 +0400

    remove unused files and configurations across multiple projects

--- vite-rtk/src/redux/store.ts
+++
@@ -1,9 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit'
-import {goodsApi} from './goodsApi'
-
-export const store = configureStore({
-    reducer: {
-        [goodsApi.reducerPath]: goodsApi.reducer,
-    },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(goodsApi.middleware)
-});
