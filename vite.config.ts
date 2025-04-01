18bc81883f0a20b6d316b60feb058521e3411ee6
Aiusha <ayushamikhaylov@gmail.com>
2025-04-01 13:46:14 +0400

    add: integrate polyfills for Node globals and modules in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,41 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import svgr from 'vite-plugin-svgr'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+import tailwindcss from "@tailwindcss/vite";
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
+import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
+import rollupNodePolyFill from "rollup-plugin-node-polyfills";
+import inject from "@rollup/plugin-inject";
 
 export default defineConfig({
   plugins: [
     react(),
     svgr(),
     tailwindcss(),
+    inject({ Buffer: ["buffer", "Buffer"] }),
   ],
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: "globalThis",
+      },
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true,
+        }),
+        NodeModulesPolyfillPlugin(),
+      ],
+    },
+  },
   build: {
     outDir: "dist",
+    rollupOptions: {
+      plugins: [rollupNodePolyFill()],
+    },
   },
-  define: {
-    global: {},
-    crypto: crypto,
+  resolve: {
+    alias: {
+      crypto: "crypto-browserify",
+    },
   },
 });
