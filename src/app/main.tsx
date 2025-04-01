90f78a62295065a50b39da1c566d777656da1483
Aiusha <ayushamikhaylov@gmail.com>
2025-04-01 14:24:52 +0400

    refactor: remove Buffer polyfill from main.tsx; update Vite configuration to include Tailwind CSS and clean up package.json

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,5 +1,3 @@
-import { Buffer } from 'buffer';
-window.Buffer = Buffer;
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles.css";
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,16 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import svgr from "vite-plugin-svgr";
-import path from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: "/test/",
-  plugins: [react(), svgr()],
+  plugins: [
+    react(),
+    svgr(),
+    tailwindcss(),
+  ],
   build: {
     outDir: "dist",
   },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  
 });
